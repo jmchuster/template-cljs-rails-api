5523a03695f345879f4cfa805c2c407fed868530
Jonathan Chu <jmchuster@gmail.com>
2016-06-06 23:20:05 -0700

    log to stdout

--- api/config/application.rb
+++ api/config/application.rb
@@ -28,5 +28,15 @@ module MyApi
     config.api_only = true
 
     config.middleware.use ActionDispatch::Cookies
+    
+    # Use a different logger for distributed setups.
+    # require 'syslog/logger'
+    # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
+
+    if ENV["RAILS_LOG_TO_STDOUT"].present?
+      logger           = ActiveSupport::Logger.new(STDOUT)
+      logger.formatter = config.log_formatter
+      config.logger = ActiveSupport::TaggedLogging.new(logger)
+    end
   end
 end
