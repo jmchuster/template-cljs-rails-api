7fc57e3903e3ef2fd63bd48d58c3737f1b686bf4
Jonathan Chu <jmchuster@gmail.com>
2016-06-06 23:38:36 -0700

    render list of users

--- api/app/controllers/users_controller.rb
+++ api/app/controllers/users_controller.rb
@@ -2,7 +2,17 @@ class UsersController < ApplicationController
   before_action :authenticate_user_token!
 
   def index
-    render plain: "Current user id is #{current_user.id}"
+    render json: {
+      data: [
+        User.all.map do |user|
+          {
+            type: 'user',
+            id: user.id,
+            email: user.email
+          }
+        end
+      ]
+    }
   end
 
 end
