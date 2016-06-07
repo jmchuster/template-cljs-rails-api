ada0a6364b3dbf256fd2986f368c1c89a5ca67dd
Jonathan Chu <jmchuster@gmail.com>
2016-06-07 16:06:09 -0700

    post against server to get user token

--- web/src/cljs/web/controls.cljs
+++ web/src/cljs/web/controls.cljs
@@ -1,11 +1,25 @@
 (ns web.controls
   (:require [reagent.core :as r]
             [web.session :as session]
-            [web.utils :as utils]))
+            [web.utils :as utils]
+            [ajax.core :refer [GET POST ajax-request]]))
 
 (defn login-user! [username password]
-  (session/set-user! {:username username :password password})
-  (if (= (utils/current_url) "/login") (utils/redirect_url! "/")))
+  (POST "/api/sessions"
+    {:params {:username username
+              :password password}
+     :format :json
+     :response-format :json
+     :keywords? true
+     :handler
+      (fn [response]
+        (.log js/console (str response))
+        (session/set-user! (:data response))
+        (if (= (utils/current_url) "/login") (utils/redirect_url! "/")))
+     :error-handler
+      (fn [{response :response} result]
+        (.log js/console (str response))
+        (js/alert (:detail (nth (:errors response) 0))))}))
 
 (defn logout-user! []
   (session/set-user! nil))
